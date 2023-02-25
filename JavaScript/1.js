a7de537a9bb3e725e1fdf2c77406cf450e88ce2e
Mana <125140084+Pochii-myjob@users.noreply.github.com>
2023-02-26 00:53:31 +0900

    Jsが動いた！

---
+++ JavaScript/1.js
@@ -0,0 +1,7 @@
+ function soushin() {
+    if(window.confirm("送信しますか？")){
+        alert("まだ送信できるようなコードは書けません。こちらのアドレスまでご連絡ください。【myjob.ym24@gmail.com】");
+    } else {
+        alert("送信しませんでした。");
+    }
+ }
